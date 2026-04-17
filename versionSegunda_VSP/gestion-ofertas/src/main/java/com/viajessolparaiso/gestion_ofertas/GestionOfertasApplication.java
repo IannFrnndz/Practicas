270f55eb4c5912ae97626e79e9f7e4070ef7d174
Ian <iannomore5@gmail.com>
2026-04-17 12:06:28 +0200

    de dolares a euros

--- versionSegunda_VSP/gestion-ofertas/src/main/java/com/viajessolparaiso/gestion_ofertas/GestionOfertasApplication.java
+++ versionSegunda_VSP/gestion-ofertas/src/main/java/com/viajessolparaiso/gestion_ofertas/GestionOfertasApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class GestionPacientesApplication {
+public class GestionOfertasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionPacientesApplication.class, args);
+		SpringApplication.run(GestionOfertasApplication.class, args);
 
 	}
 
--- versionSegunda_VSP/gestion-ofertas/src/test/java/com/viajessolparaiso/gestion_ofertas/GestionOfertasApplicationTests.java
+++ versionSegunda_VSP/gestion-ofertas/src/test/java/com/viajessolparaiso/gestion_ofertas/GestionOfertasApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionPacientesApplicationTests {
+class GestionOfertasApplicationTests {
 
 	@Test
 	void contextLoads() {
